313f9721edd84050000630bd306ad3b5ceee1f12
SanchunPeng <1024884152@qq.com>
2017-05-21 00:36:10 +0800

    add

---
+++ js/index.js
@@ -0,0 +1,13 @@
+$(function(){
+	//搜索框隐藏和显示
+	$(".header_nav_search_show").click(function(){
+		$(".header_nav_search").show();
+		$(".header_nav_search_show").hide();
+	});
+	$(".header_nav_search_btn").click(function(){
+		$(".header_nav_search").hide();
+		$(".header_nav_search_show").show();
+	})
+	
+	
+})
