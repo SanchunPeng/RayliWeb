7b31484f327df4bd143c31d7520be87046e9ddab
SanchunPeng <1024884152@qq.com>
2017-05-21 22:59:35 +0800

    continue

--- js/index.js
+++ js/index.js
@@ -15,7 +15,13 @@ $(function(){
 		_index=$(this).index();
 		$(this).addClass("banner_li_hover").siblings().removeClass("banner_li_hover");
 		$(".banner_container").animate({left:-_index*850},500);
-	})
+	});
+	
+	$("#banner_next_img").mouseover(function(){
+		$(this).css({'background':'rgba(234, 46, 46, 0.7)'});
+	}).mouseout(function(){
+		$(this).css({'background':'rgba(0, 0, 0, 0)'});
+	});
 	$("#banner_next_img").click(function(){
 		_index++;
 		if(_index>8){
@@ -24,6 +30,11 @@ $(function(){
 		}
 		$(".banner_btn ul li").eq(_index).addClass("banner_li_hover").siblings().removeClass("banner_li_hover");
 		$(".banner_container").animate({left:-_index*850},500);
+	});
+	$("#banner_prev_img").mouseover(function(){
+		$(this).css({'background':'rgba(234, 46, 46, 0.7)'});
+	}).mouseout(function(){
+		$(this).css({'background':'rgba(0, 0, 0, 0)'});
 	});
 	$("#banner_prev_img").click(function(){
 		_index--;
@@ -34,5 +45,38 @@ $(function(){
 		$(".banner_btn ul li").eq(_index).addClass("banner_li_hover").siblings().removeClass("banner_li_hover");
 		$(".banner_container").animate({left:-_index*850},500);
 	});
+	
+	//content_one_left_top时装专栏
+	$(".content_one_left_topBtn ul li").mouseover(function(){
+		$(this).addClass("one_left_top_hover").siblings().removeClass("one_left_top_hover");
+		var index_top=$(this).index();
+		$(".content_one_left_top ul li").eq(index_top).fadeIn().siblings("li").fadeOut();
+	});
+	
+	//content_one_left_bottom
+	var index_bottom=0;
+	var time_bottom=null;
+	$("ul.content_one_left_bottom_flashBtn li").mouseover(function(){
+		clearInterval(time_bottom);
+		index_bottom=$(this).index();
+		$(this).addClass("one_left_bottom_hover").siblings().removeClass("one_left_bottom_hover");
+		$(".content_one_left_bottom_flash").stop().animate({left:-index_bottom*280},400);
+	})
+	$("ul.content_one_left_bottom_flashBtn li").mouseout(function(){
+		autoFlash();
+	})
+	autoFlash();
+	function autoFlash(){
+		time_bottom=setInterval(function(){
+			index_bottom++;
+			if(index_bottom>3){
+				index_bottom=0;
+			}
+			$("ul.content_one_left_bottom_flashBtn li").eq(index_bottom).addClass("one_left_bottom_hover").siblings().removeClass("one_left_bottom_hover");
+			$(".content_one_left_bottom_flash").stop().animate({left:-index_bottom*280},400);
+		},2000);
+		
+		
+	}
 	
 })
